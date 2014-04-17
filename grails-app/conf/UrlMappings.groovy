2579c2f7dda8b17985002c0a302fb2c439411417
Daniel Nagy <dn686910@ohio.edu>
2014-04-16 23:25:22 -0400

    CRUD

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -6,6 +6,15 @@ class UrlMappings {
                 // apply constraints here
             }
         }
+        
+        // CRUD
+        "/album"(controller: "album"){
+            action = [POST: "create", GET: "list"]
+        }
+
+        "/album/$title"(controller: "album"){
+        	action = [GET: "retrieve", PUT: "update", DELETE: "destroy"]
+        }
 
         "/"(view:"/index")
         "500"(view:'/error')
--- grails-app/controllers/album/collection/AlbumController.groovy
+++ grails-app/controllers/album/collection/AlbumController.groovy
@@ -1,7 +1,34 @@
 package album.collection
 
+import grails.converters.JSON
+
 class AlbumController {
 
-    // let Grails handle the REST API for now
-    def scaffold = Album
+    def create(){
+        def album = new Album(params)
+        album.save()
+        render album as JSON
+    }
+
+    def list(){
+        render Album.list() as JSON
+    }
+    
+    def retrieve(){
+        def album = Album.findByTitle(params.title)
+        render album as JSON
+    }
+    
+    def update(){
+        def album = Album.findByTitle(params.title)
+        album.properties = params
+        album.save()
+        render album as JSON
+    }
+    
+    def destroy(){
+        def album = Album.findByTitle(params.title)
+        album.delete()
+        render "album deleted"
+    }
 }
