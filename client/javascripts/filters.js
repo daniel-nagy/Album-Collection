40bcdf95b04ca4316c5d7587de8b550f6131e018
Daniel Nagy <dn686910@ohio.edu>
2014-05-12 22:08:28 -0400

    Using CSS column-count instead of filtering the track-listing into separate lists.

--- client/javascripts/filters.js
+++ client/javascripts/filters.js
@@ -34,27 +34,3 @@ albumFilters.filter('capitalizeFilter', function () {
     return input ? input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase() : input;
   };
 });
-
-/* 
- * Returns the first half of an album's
- * track-listing.
- */
-
-albumFilters.filter('firstHalfFilter', function () {
-  
-  return function (input) {
-    return input ? input.slice(0, Math.ceil(input.length / 2)) : input;
-  }
-});
-
-/* 
- * Returns the second half of an album's
- * track-listing.
- */
-
-albumFilters.filter('secondHalfFilter', function () {
-  
-  return function (input) {
-    return input ? input.slice(Math.ceil(input.length / 2)) : input;
-  }
-});
